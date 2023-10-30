9980eb9179f885eba7eb8570780231c89c592089
R Kanishkasree <124056440+KanishkasreeR@users.noreply.github.com>
2023-10-30 12:35:17 +0530

    Add files via upload

---
+++ Boyer_Moore.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define TOTAL_CHARACTERS 256
+
+void badMatch(string s, int n,int badCharacter[TOTAL_CHARACTERS]) {
+   for (int i = 0; i < TOTAL_CHARACTERS; i++)
+      badCharacter[i] = -1;
+   for (int i = 0; i < n; i++)
+      badCharacter[(int)s[i]] = i;
+}
+
+void _search(string text, string pattern) {
+   int n = text.size();
+   int m = pattern.size();
+   int badCharacter[TOTAL_CHARACTERS];
+   badMatch(pattern, m, badCharacter);
+   int s = 0;
+   while (s <= (n - m)) {
+      int j = m - 1;
+      while (j >= 0 && pattern[j] == text[s + j])
+         j--;
+
+      if (j < 0) {
+         cout << "Pattern found";
+         break;
+
+      }
+      else
+         s += j - badCharacter[text[s + j]];
+         cout<<"pos: "<<s;
+   }
+}
+
+int main() {
+   string text = "TRUSTHARDTOOTHBRUSH";
+   string pattern = "TOOTH";
+   _search(text, pattern);
+   return 0;
+}
